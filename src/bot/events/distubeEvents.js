452fec65d4854044acf0a09673def702e47df17f
簡毓芳 <jianyufang@jianyufangs-MacBook-Air.local>
2026-02-05 15:52:25 +0800

    Fix: Add missing brace in distubeEvents.js

--- src/bot/events/distubeEvents.js
+++ src/bot/events/distubeEvents.js
@@ -31,3 +31,4 @@ module.exports = (client) => {
             if (channel) channel.send(`❌ An error encountered: ${e.toString().slice(0, 1974)}`);
             else console.error(e);
         });
+};
